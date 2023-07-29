0aa7afd4daa8f47148bd3f6b5354e25036659382
Magnus Kulke <magnuskulke@microsoft.com>
2023-07-29 15:50:34 +0200

    Split out kbs uri parsing into dedicated type
    Split uri parsing into its own type/parsing fn's to be
    able to unit test parsing w/o having to creating a Kbc
    with side-effects.
    
    Signed-off-by: Magnus Kulke <magnuskulke@microsoft.com>

--- attestation-agent/kbc/src/cc_kbc/mod.rs
+++ attestation-agent/kbc/src/cc_kbc/mod.rs
@@ -13,11 +13,51 @@ use anyhow::*;
 use async_trait::async_trait;
 use base64::Engine;
 use resource_uri::ResourceUri;
-use url::Url;
+use std::convert::TryFrom;
+use url::{Host, Url};
 use zeroize::Zeroizing;
 
+#[derive(Debug)]
+pub struct KbsUri {
+    url: Url,
+    host: Host,
+}
+
+impl KbsUri {
+    fn host(&self) -> &Host {
+        &self.host
+    }
+
+    fn as_str(&self) -> &str {
+        self.url.as_str().trim_end_matches('/')
+    }
+
+    pub fn with_resource(&self, resource: &ResourceUri) -> Result<String> {
+        let kbs_addr = if let Some(port) = self.url.port() {
+            format!("{}:{port}", self.host)
+        } else {
+            self.host.to_string()
+        };
+
+        if !resource.kbs_addr.is_empty() && resource.kbs_addr != kbs_addr {
+            bail!(
+                "The resource KBS host {} differs from the KBS URL one {kbs_addr}",
+                resource.kbs_addr
+            );
+        }
+
+        let kbs_addr = &self.as_str();
+        let repo = &resource.repository;
+        let r#type = &resource.r#type;
+        let tag = &resource.tag;
+        Ok(format!(
+            "{kbs_addr}{KBS_PREFIX}/resource/{repo}/{type}/{tag}"
+        ))
+    }
+}
+
 pub struct Kbc {
-    kbs_uri: Url,
+    kbs_uri: KbsUri,
     token: Option<String>,
     kbs_protocol_wrapper: KbsProtocolWrapper,
 }
@@ -51,23 +91,35 @@ impl KbcInterface for Kbc {
     }
 }
 
+impl TryFrom<&str> for KbsUri {
+    type Error = Error;
+
+    fn try_from(kbs_url: &str) -> Result<Self> {
+        let url = Url::parse(&kbs_url).map_err(|e| anyhow!("Invalid URL {kbs_url}: {e}"))?;
+        let Some(host) = url.host() else {
+            bail!("{kbs_url} is missing a host");
+        };
+        let host = host.to_owned();
+
+        let kbs_uri = KbsUri { url, host };
+        Ok(kbs_uri)
+    }
+}
+
 impl Kbc {
     pub fn new(kbs_uri: String) -> Result<Kbc> {
         // Check the KBS URI validity
-        let url = Url::parse(&kbs_uri).map_err(|e| anyhow!("Invalid URI {kbs_uri}: {e}"))?;
-        if !url.has_host() {
-            bail!("{kbs_uri} is missing a host");
-        }
+        let kbs_uri = kbs_uri.as_str().try_into()?;
 
         Ok(Kbc {
-            kbs_uri: url,
+            kbs_uri,
             token: None,
             kbs_protocol_wrapper: KbsProtocolWrapper::new(vec![]).unwrap(),
         })
     }
 
     fn kbs_uri(&self) -> &str {
-        self.kbs_uri.as_str().trim_end_matches('/')
+        self.kbs_uri.as_str()
     }
 
     fn kbs_protocol_wrapper(&mut self) -> &mut KbsProtocolWrapper {
@@ -76,44 +128,22 @@ impl Kbc {
 
     /// Convert a [`ResourceUri`] to a KBS URL.
     pub fn resource_to_kbs_uri(&self, resource: &ResourceUri) -> Result<String> {
-        let kbs_host = self
-            .kbs_uri
-            .host_str()
-            .ok_or_else(|| anyhow!("Invalid URL: {}", self.kbs_uri))?;
-
-        let kbs_addr = if let Some(port) = self.kbs_uri.port() {
-            format!("{kbs_host}:{port}")
-        } else {
-            kbs_host.to_string()
-        };
-
-        if !resource.kbs_addr.is_empty() && resource.kbs_addr != kbs_addr {
-            bail!(
-                "The resource KBS host {} differs from the KBS URL one {kbs_addr}",
-                resource.kbs_addr
-            );
-        }
-
-        let kbs_addr = self.kbs_uri();
-        let repo = &resource.repository;
-        let r#type = &resource.r#type;
-        let tag = &resource.tag;
-        Ok(format!(
-            "{kbs_addr}{KBS_PREFIX}/resource/{repo}/{type}/{tag}"
-        ))
+        self.kbs_uri.with_resource(resource)
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::ResourceUri;
-    use crate::cc_kbc::Kbc;
+    // use super::ResourceUri;
+    // use crate::cc_kbc::KbsUri;
+    use super::*;
 
     const RESOURCE_URL_PORT: &str = "kbs://127.0.0.1:8081/alice/cosign-key/213";
     const RESOURCE_URL_NO_PORT: &str = "kbs://127.0.0.1/alice/cosign-key/213";
     const RESOURCE_NO_HOST_URL: &str = "kbs:///alice/cosign-key/213";
 
     const KBS_URL_PORT: &str = "https://127.0.0.1:8081";
+    const KBS_URL_NO_HOST: &str = "file:///tmp/wrong";
     const KBS_URL_NO_PORT: &str = "https://127.0.0.1";
     const KBS_INVALID_URL: &str = "kbs:///alice/cosign-key/213";
 
@@ -122,30 +152,31 @@ mod tests {
     const RESOURCE_KBS_URL_NO_PORT: &str = "https://127.0.0.1/kbs/v0/resource/alice/cosign-key/213";
 
     #[test]
-    fn new_invalid_uri() {
-        let kbc = Kbc::new(KBS_INVALID_URL.to_string());
-        assert!(kbc.is_err());
+    fn invalid_uri() {
+        let kbs_uri: Result<KbsUri> = KBS_INVALID_URL.try_into();
+        assert!(kbs_uri.is_err());
     }
 
     #[test]
-    fn new_valid_uri() {
-        let kbc = Kbc::new(KBS_URL_PORT.to_string());
-        assert!(kbc.is_ok());
+    fn no_host() {
+        let kbs_uri: Result<KbsUri> = KBS_URL_NO_HOST.try_into();
+        assert!(kbs_uri.is_err());
+    }
+
+    #[test]
+    fn valid_uri() {
+        let kbs_uri: Result<KbsUri> = KBS_URL_PORT.try_into();
+        assert!(kbs_uri.is_ok());
     }
 
     fn to_kbs_uri(kbs_url: &str, resource_url: &str, expected_kbs_url: &str) {
         let resource: ResourceUri =
             serde_json::from_str(&format!("\"{resource_url}\"")).expect("deserialize failed");
 
-        let kbc = Kbc::new(kbs_url.to_string());
-        assert!(kbc.is_ok());
+        let kbs_uri: KbsUri = kbs_url.try_into().unwrap();
 
-        println!(
-            "{} {:?}",
-            resource.kbs_addr,
-            kbc.as_ref().unwrap().kbs_uri()
-        );
-        let resource_kbs_url = kbc.unwrap().resource_to_kbs_uri(&resource);
+        println!("{} {:?}", resource.kbs_addr, kbs_uri);
+        let resource_kbs_url = kbs_uri.with_resource(&resource);
 
         assert!(resource_kbs_url.is_ok());
         assert_eq!(resource_kbs_url.unwrap(), expected_kbs_url);
